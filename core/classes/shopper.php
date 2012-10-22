e504f900c62328cf8a4fb401fab48d3211d6034a
Kevin Baugh <loraxx753@gmail.com>
2012-10-21 22:39:41 -0400

    added comments for the shopper class

--- core/classes/shopper.php
+++ core/classes/shopper.php
@@ -10,7 +10,11 @@ class Shopper {
 	{
 		$this->cart = $cart;
 	}
-
+	/**
+	 * Loads a new instance of a cart (call this first, like $cart = Shopper::load)
+	 * then do all your stuff to $cart
+	 * @return class returns a new Shopper class loaded with the cart.
+	 */
 	static public function load() {
 		if(!Session::get('cart')) {
 			Session::set('cart', array());
@@ -18,6 +22,10 @@ class Shopper {
 		return new Shopper(Session::get('cart'));
 	}
 
+	/**
+	 * Adds a new item to the cart.
+	 * @param int $item_id the ID of the item that should be added to the cart.
+	 */
 	public function add_item($item_id)
 	{
 		if(!in_array($item_id, $this->cart))
@@ -32,14 +40,29 @@ class Shopper {
 		}
 	}
 
+	/**
+	 * Removes an item from the cart.
+	 * @param  int $item_id  the id of the item to be removed
+	 * @return bool          true on removal, false if something goes wrong
+	 */
 	public function remove_item($item_id) 
 	{
 		$key = array_search($item_id, $this->cart);
-		unset($this->cart[$key]);
+		try {
+			unset($this->cart[$key]);
+		}
+		catch(Exception $e)
+		{
+			return false;
+		}
 		Session::set('cart', $this->cart);
 		return true;
 	}
 
+	/**
+	 * Clears the cart of all items
+	 * @return bool returns true on removal.
+	 */
 	public function clear() 
 	{
 		$this->cart = array();
@@ -47,6 +70,10 @@ class Shopper {
 		return true;
 	}
 
+	/**
+	 * Gets all of the items currently in the cart.
+	 * @return array An array of Model_Product classes that are in the cart.
+	 */
 	public function get()
 	{
 		if(count($this->cart) > 0)
@@ -65,6 +92,11 @@ class Shopper {
 		}
 	}
 
+	/**
+	 * Searches the cart for a specific ID
+	 * @param  int $search_id The ID to look for within the cart
+	 * @return bool           True if the item was found, false if it wasn't.
+	 */
 	public function search($search_id)
 	{
 		if(in_array($search_id, $this->cart))
@@ -77,25 +109,36 @@ class Shopper {
 		}
 	}
 
+	/**
+	 * Finds the subtotal of all the items in the cart.
+	 * @return int The price of all the items in the cart.
+	 */
 	public function subtotal()
 	{
-		$products = \Model_Products::build();
-		foreach($this->cart as $item)
+		if(count($this->cart) > 0)
 		{
-			$products->or_where('ProductID', $item);
+			$products = \Model_Products::build();
+			foreach($this->cart as $item)
+			{
+				$products->or_where('ProductID', $item);
+			}
+			$products->selector('Price');
+
+			$prices = $products->execute();
+
+			$subtotal = 0;
+
+			foreach($prices as $price)
+			{
+				$subtotal += (int)$price->Price;
+			}
+			
+			return $subtotal;
 		}
-		$products->selector('Price');
-
-		$prices = $products->execute();
-
-		$subtotal = 0;
-
-		foreach($prices as $price)
+		else
 		{
-			$subtotal += (int)$price->Price;
+			return 0;
 		}
-		
-		return $subtotal;
 	}
 
 }
