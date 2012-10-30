69e59c0159a92e9028cf4cc1f5bd9ced2d971615
JFerraro <j.ferraro@hotmail.com>
2012-10-29 20:49:55 -0400

    Removed ad section, expanded featured item sections.

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -6,7 +6,7 @@
 		<div id="main_content" class="row">
 			<div class="span8">
 				<div class="row">
-					<div class="span9 featured">
+					<div class="span12 featured">
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/chair2_thumbnail.jpg", "Wooden Chair")?></li>
@@ -28,11 +28,18 @@
 								<li>$100.00</li>
 							</ul>
 						</div>
-						<div class="span2 offset6">
+						<div class="span2 catalog_item">
+							<ul class="thumbnail">
+								<li><?=Load::image("/products/chair2_thumbnail.jpg", "Wooden Chair")?></li>
+								<li><?=Load::link(array('product' => "Napoleon Chair"));?></li>
+								<li>$100.00</li>
+							</ul>
+						</div>
+						<div class="span2 offset9">
 							<button class="btn btn-small featured_button">More Featured Items</button>
 						</div>						
 					</div>
-					<div class="span9 featured">
+					<div class="span12 featured">
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/couch2_thumbnail.jpg", "Chic Couch")?></li>
@@ -54,12 +61,18 @@
 								<li>$100.00</li>
 							</ul>
 						</div>
-
-						<div class="span2 offset6">
+						<div class="span2 catalog_item">
+							<ul class="thumbnail">
+								<li><?=Load::image("/products/couch2_thumbnail.jpg", "Chic Couch")?></li>
+								<li><?=Load::link(array('product' => "Chic Couch"));?></li>
+								<li>$100.00</li>
+							</ul>
+						</div>
+						<div class="span2 offset9">
 							<button class="btn btn-small featured_button">More Top Sellers</button>
 						</div>	
 					</div>
-					<div class="span9 featured">
+					<div class="span12 featured">
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/desk_thumbnail.jpg", "Apple Desk")?></li>
@@ -81,19 +94,16 @@
 								<li>$100.00</li>
 							</ul>
 						</div>
-
-						<div class="span2 offset6">
+						<div class="span2 catalog_item">
+							<ul class="thumbnail">
+								<li><?=Load::image("/products/desk_thumbnail.jpg", "Apple Desk")?></li>
+								<li><?=Load::link(array('product' => "Apple Desk"));?></li>
+								<li>$100.00</li>
+							</ul>
+						</div>
+						<div class="span2 offset9">
 							<button class="btn btn-small featured_button">More Newly Added</button>
 						</div>	
-					</div>
-				</div>
-			</div>
-			<div class="span3 offset1">
-				<div class="row">
-					<div id="ad_sidebar" class="span3">
-						<?=Load::image('ads/ad1.jpg', "Marketing Firm Ad")?>
-						<?=Load::image('ads/ad2.jpeg', "McDonalds Ad")?>
-						<?=Load::image('ads/ad3.jpeg', "Apple Ad")?>
 					</div>
 				</div>
 			</div>
