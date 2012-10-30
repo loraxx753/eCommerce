f9c7c2b2238286b5b5f0263d5947047afa667000
JFerraro <j.ferraro@hotmail.com>
2012-10-29 21:39:39 -0400

    Added footer link content

--- app/views/template.php
+++ app/views/template.php
@@ -35,6 +35,7 @@
 		<?php require_once($page_location); ?>
 		<div class="row">
 			<footer id="footer" class="span12">
+				<div id="footer_links" class="span12"><a href="#">About Us</a> | <a href="#">Policies</a></div>
 				<span id="disclaimer" class="span12">
 					This site is not official and is an assignment for a UCF Digital Media course.<br />
 					Designed by NWA Furniture
