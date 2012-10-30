f10ad08a257c2bc5c233977acac085329c3ffff8
JFerraro <j.ferraro@hotmail.com>
2012-10-29 20:46:54 -0400

    Updated buttons, nav, footer tags and content.

--- app/views/template.php
+++ app/views/template.php
@@ -1,6 +1,4 @@
-<!-- Starting the HTML -->
-<!-- DISCLAIMER: I used Twitter Bootstrap for their grid system.  Bootstrap comes with basic styles built in, however, they have been changed in my stylesheet -->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -13,14 +11,10 @@
 			<div id="logo" class="span5">
 				<h1>NWA Furniture</h1>
 			</div>
-			<div id="admin_buttons" class="span4 offset3">
-				<form method="get" action="admin.php">
-					<button class="btn btn-small">Manage</button>
-				</form>
-				<form method="get" action="client.php">
-					<button class="btn btn-small">My Account</button>
-				</form>
-				<button class="btn btn-small">Log-In</button>
+			<div id="admin_buttons" class="span5 offset2">
+				<a href="<?=WEB_BASE?>admin" class="btn btn-small">Manage</a>
+				<a href="<?=WEB_BASE?>client"class="btn btn-small">My Account</a>
+				<a class="btn btn-small">Log-In</a>
 			</div>
 			<div id="search" class="span7">
 				<form class="form-search pull-right" action="#">
@@ -30,20 +24,22 @@
 				</form>
 			</div>
 		</div>
-		<div id="nav" class="row">
-				<div class="span3"><a href="<?=WEB_BASE?>">HOME</a></div>
-				<div class="span3"><a href="<?=WEB_BASE?>catalog">PRODUCTS</a></div>
-				<div class="span3"><a href="<?=WEB_BASE?>cart">CART</a></div>
-				<div class="span3"><a href="<?=WEB_BASE?>checkout">CHECKOUT</a></div>
-		</div>
+		<nav id="nav" class="row">
+			<ul>
+				<li class="span3"><a href="<?=WEB_BASE?>">HOME</a></li>
+				<li class="span3"><a href="<?=WEB_BASE?>catalog">PRODUCTS</a></li>
+				<li class="span3"><a href="<?=WEB_BASE?>cart">CART</a></li>
+				<li class="span3"><a href="<?=WEB_BASE?>checkout">CHECKOUT</a></li>
+			</ul>
+		</nav>
 		<?php require_once($page_location); ?>
 		<div class="row">
-			<div id="footer" class="span12">
+			<footer id="footer" class="span12">
 				<span id="disclaimer" class="span12">
 					This site is not official and is an assignment for a UCF Digital Media course.<br />
-					Designed by Jesse Ferraro
+					Designed by NWA Furniture
 				</span>
-			</div>
+			</footer>
 		</div>
 	</div>
 	<?=Load::js(array('//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', "bootstrap.js"));?>
