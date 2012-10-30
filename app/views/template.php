e7f4fff5890f424648211e2f90f8d35340ddf1b0
JFerraro <j.ferraro@hotmail.com>
2012-10-29 21:19:05 -0400

    Added Google Analytics code.

--- app/views/template.php
+++ app/views/template.php
@@ -43,5 +43,18 @@
 		</div>
 	</div>
 	<?=Load::js(array('//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', "bootstrap.js"));?>
+
+<!-- Google Analytics -->
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-15417266-13']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 </body>
 </html>
