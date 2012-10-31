57461e8b7788995c7de25160ccdcf9b7a70ad402
JFerraro <j.ferraro@hotmail.com>
2012-10-30 20:27:32 -0400

    Added member roles and company headquarter information.

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,18 @@
 <div class="span12">
-	<h1>Test Content</h1>
+	<h2>About Our Company</h2>
+	<div class="span5">
+		<ul>
+			<li><strong>Jesse Ferraro</strong> - Front End</li>
+			<li><strong>Rodney Ritchey</strong> - Front End</li>
+			<li><strong>Liat Stilman</strong> - Front End</li>
+			<li><strong>Kevin Baugh</strong> - Back End</li>
+			<li><strong>Chris McLean</strong> - Back End</li>
+		</ul>
+	</div>
+	<div class="span5 offset1">
+		<p><strong>NWA Furniture</strong></p>
+		<p>4000 Central Florida Blvd<br />
+			Orlando, FL 32816<br/ >
+			407-823-0000</p>
+	</div>
 </div>
