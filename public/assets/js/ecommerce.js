96422eecfbfbbe0bf3938f66a95243951734d037
JFerraro <j.ferraro@hotmail.com>
2012-11-04 17:25:02 -0500

    Adding login dialog box code.

--- public/assets/js/ecommerce.js
+++ public/assets/js/ecommerce.js
@@ -1,3 +1,16 @@
+var $loginDialog = $('#loginBox').dialog({
+	autoOpen: false,
+	draggable: false,
+	resizable: false,
+	width: 400,	
+	buttons: {
+		'Save' : function(e) {editMember(e);},
+		'Cancel': function(e) {closeNotesDialog(e);}
+	},
+	modal: true
+});
+
 $('#login').on('click', function (){
+	console.log('clicked');
 	$('#loginBox').dialog('open');
 });
