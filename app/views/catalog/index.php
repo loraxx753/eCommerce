e675901f710d9e0c32b1f9c9cc36b92cad7e26bf
mclean.cw <havok2905@gmail.com>
2012-11-05 23:10:23 -0500

    changed product links from use of anchor tags to use of the Load class

--- app/views/catalog/index.php
+++ app/views/catalog/index.php
@@ -42,19 +42,19 @@
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/chair2_thumbnail.jpg", "Chairs")?></li>
-								<?= '<li>'.Load::link(array('catalog/catagories/1' => "Chairs")).'</li>';?>
+								<li><?=Load::link(array('catalog/catagories/1' => "Chairs"))?></li>
 							</ul>
 						</div>
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/couch2_thumbnail.jpg", "Couches")?></li>
-								<?= '<li>'.Load::link(array('catalog/catagories/2' => "Couches")).'</li>';?>
+								<li><?=Load::link(array('catalog/catagories/2' => "Couches"))?></li>
 							</ul>
 						</div>
 						<div class="span2 catalog_item">
 							<ul class="thumbnail">
 								<li><?=Load::image("/products/desk2_thumbnail.jpg", "Desks")?></li>
-								<?= '<li>'.Load::link(array('catalog/catagories/3' => "Desks")).'</li>';?>
+								<li><?=Load::link(array('catalog/catagories/3' => "Desks"))?></li>
 							</ul>
 						</div>		
 					</div>
@@ -67,8 +67,8 @@
 								
 								echo '<div class="span2 catalog_item">';
 								echo '<ul class="thumbnail">';
-								echo '<li>'.Load::image($value->Product_Image."_thumbnail.jpg", "Chairs").'</li>';
-								echo '<li><a href="product/'.$value->ProductID.'">'.$value->Product_Name.'</a></li>';
+								echo '<li>'.Load::image($value->Product_Image."_thumbnail.jpg", "Chairs").'</li>';	
+								echo '<li>'.Load::link(array('product/'.$value->ProductID => $value->Product_Name)).'</li>';
 								echo '<li>'.number_format($value->Product_Price,2).'</li>';
 								echo '</ul>';
 								echo '</div>';
