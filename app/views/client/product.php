e2c0a8b5348cf61b2d01381a65253245c8215836
Kevin Baugh <loraxx753@gmail.com>
2012-11-08 23:34:30 -0500

    changed some of the html in client product

--- app/views/client/product.php
+++ app/views/client/product.php
@@ -20,43 +20,40 @@
 			<div class="span8">
 				<div class="row">
 					<div id="admin_dashboard" class="span8">
-<<<<<<< HEAD
-							<h3>Manage Products</h3>
-							<small>Manage all of your store products!</small>
-						<div class="span8">
-							<h5>Content Goes Here</h5>
-=======
 						<div id="order_info" class="span8">
 							<form id="contact" method="post" action="#">
                 	<fieldset>
                     	<legend><h3>Catalog Portal</h3></legend>
                             <label>Product ID:</label>
-                            	<input type="text" size="30" name="ProductID" value="Enter product ID here" />
+                            	<input type="text" size="30" name="ProductID" placeholder="Enter product ID here" />
                            <label>SKU:</label>
-                           	<input type="text" size="30" name="SKU" value="Enter SKU here" />
+                           	<input type="text" size="30" name="SKU" placeholder="Enter SKU here" />
                            <label>Product Description:</label>
-                           	<teatarea rows="20" cols="50"></textarea>
+                           	<textarea rows="20" cols="50"></textarea>
                            	<label>Category ID:</label>
-                           	<input type="text" size="30" name="Category_ID" value="Enter Category ID here" />
+                           	<input type="text" size="30" name="Category_ID" placeholder="Enter Category ID here" />
                            	<label>Stock:</label>
-                           	<input type="text" size="30" name="Stock" value="Enter stock amount here" />
+                           	<input type="text" size="30" name="Stock" placeholder="Enter stock amount here" />
                            	<label>Cost:</label>
-                           	<input type="text" size="30" name="Product_Cost" value="Enter product cost here" />
+                           	<input type="text" size="30" name="Product_Cost" placeholder="Enter product cost here" />
                            	<label>Product Price:</label>
-                           	<input type="text" size="30" name="Product_Price" value="Enter product price here" />
+                           	<input type="text" size="30" name="Product_Price" placeholder="Enter product price here" />
                            	<label>Product Image:</label>
-                           	<input type="text" size="30" name="Product_Image" value="Enter image url here" />
+                           	<input type="text" size="30" name="Product_Image" placeholder="Enter image url here" />
                            	<label>Product Weight:</label>
-                           	<input type="text" size="30" name="Weight" value="Enter product weight here" />
+                           	<input type="text" size="30" name="Weight" placeholder="Enter product weight here" />
                            	<label>Product Size:</label>
-                           	<input type="text" size="30" name="Size" value="Enter product size here" />
+                           	<input type="text" size="30" name="Size" placeholder="Enter product size here" />
                            	<label>Product Name:</label>
-                           	<input type="text" size="30" name="Product_Name" value="Enter product name here" />
+                           	<input type="text" size="30" name="Product_Name" placeholder="Enter product name here" />
                            	<label>Featured Product:</label>
-                           	<input type="radio" name="feat" value="#">
+                           	<input type="checkbox" name="feat" value="true">
+                           	<label>Image:</label>
+                           	<input type="file" name="image" />
+                           	<label>Thumbnail:</label>
+                           	<input type="file" name="thumbnail" />
                     </fieldset>
                 </form>
->>>>>>> Added product management form
 						</div>	
 					</div>
 				</div>
