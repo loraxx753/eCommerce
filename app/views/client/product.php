cd481ab56b6ce58c64eb0529be900d8697c09a0a
Rodney Ritchey <rodneyritchey@gmail.com>
2012-11-08 23:25:15 -0500

    Added product management form

--- app/views/client/product.php
+++ app/views/client/product.php
@@ -20,10 +20,43 @@
 			<div class="span8">
 				<div class="row">
 					<div id="admin_dashboard" class="span8">
+<<<<<<< HEAD
 							<h3>Manage Products</h3>
 							<small>Manage all of your store products!</small>
 						<div class="span8">
 							<h5>Content Goes Here</h5>
+=======
+						<div id="order_info" class="span8">
+							<form id="contact" method="post" action="#">
+                	<fieldset>
+                    	<legend><h3>Catalog Portal</h3></legend>
+                            <label>Product ID:</label>
+                            	<input type="text" size="30" name="ProductID" value="Enter product ID here" />
+                           <label>SKU:</label>
+                           	<input type="text" size="30" name="SKU" value="Enter SKU here" />
+                           <label>Product Description:</label>
+                           	<teatarea rows="20" cols="50"></textarea>
+                           	<label>Category ID:</label>
+                           	<input type="text" size="30" name="Category_ID" value="Enter Category ID here" />
+                           	<label>Stock:</label>
+                           	<input type="text" size="30" name="Stock" value="Enter stock amount here" />
+                           	<label>Cost:</label>
+                           	<input type="text" size="30" name="Product_Cost" value="Enter product cost here" />
+                           	<label>Product Price:</label>
+                           	<input type="text" size="30" name="Product_Price" value="Enter product price here" />
+                           	<label>Product Image:</label>
+                           	<input type="text" size="30" name="Product_Image" value="Enter image url here" />
+                           	<label>Product Weight:</label>
+                           	<input type="text" size="30" name="Weight" value="Enter product weight here" />
+                           	<label>Product Size:</label>
+                           	<input type="text" size="30" name="Size" value="Enter product size here" />
+                           	<label>Product Name:</label>
+                           	<input type="text" size="30" name="Product_Name" value="Enter product name here" />
+                           	<label>Featured Product:</label>
+                           	<input type="radio" name="feat" value="#">
+                    </fieldset>
+                </form>
+>>>>>>> Added product management form
 						</div>	
 					</div>
 				</div>
