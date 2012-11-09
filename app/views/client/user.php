069914d8e0c35847e007126de8ffa10b69ffef3d
JFerraro <j.ferraro@hotmail.com>
2012-11-08 22:08:51 -0500

    Created new pages for CRUD forms

---
+++ app/views/client/user.php
@@ -0,0 +1,37 @@
+		<div class="row">
+			<div class="span4">
+				<div class="row">
+					<div id="client_sidebar" class="span4">
+						<span>Account Home</span>
+						<ul>
+							<?='<li>'.Load::link(array('client' => "Dashboard")).'</li>';?>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="span8">
+				<div class="row">
+					<div id="dashboard" class="span8">
+							<h3>Dashboard</h3>
+							<small>A summary of all recent activity on your account.</small>
+						<div id="account_info" class="span8">
+							<h5>Account Info</h5>
+							<hr />
+							<p class="span4"><?=$email?></p>
+							<p class="span2">Account #:</p>
+							<p class="span4"><?=$name?></p>
+						</div>	
+						<div class="span8">
+							<h5>Recent Order Status</h5>
+							<hr />
+							<p class="span3">None Currently Available</p>
+						</div>	
+						<div class="span8">
+							<h5>Recent RMA Status</h5>
+							<hr />
+							<p class="span3">None Currently Available</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
